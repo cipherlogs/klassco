4ec07d476ad07a765fa98ad2c5235fda73d66bda
cipherlogs <73669345+0x78logs@users.noreply.github.com>
2023-03-15 17:21:28 -0100

    Make special flags like -v -h to have importance than others

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,7 @@ import System.Environment
 import System.Directory
 import System.FilePath ((</>))
 import Control.Monad ((<=<))
-import Data.List (isSuffixOf)
+import Data.List (isSuffixOf, isPrefixOf)
 import Data.Maybe
 import Lib
 import Logo
@@ -50,26 +50,33 @@ printMessage msg =
 
 data CliFlag =
   Fhelp |
-  Fsummary |
   Fversion |
-  Fmin
+  Fmin |
+  Fextensions
   deriving Eq
 
 parseFlag :: String -> Maybe CliFlag
 parseFlag flag
   | flag == "-h" || flag == "--help" = Just Fhelp
+  | flag == "-v" || flag == "--version" = Just Fversion
+  | flag == "-m" || flag == "--min" = Just Fmin
+  | flag == "-e" || flag == "--extensions" = Just Fextensions
   | otherwise = Nothing
 
 -- maybe rewrite this so that it can look nicer.
 -- how about using guards only
 parseArgs :: [String] -> ([CliFlag], Maybe String)
 parseArgs [] = ([Fhelp], Nothing)
+
 parseArgs [x]
-  | x == "-h" || x == "--help" = ([Fhelp], Nothing)
+  | isFlag x = (mapMaybe parseFlag [x], Nothing)
   | otherwise = ([], Just x)
+  where isFlag :: String -> Bool
+        isFlag = isPrefixOf "-"
 
 parseArgs xs
   | "-h" `elem` xs || "--help" `elem` xs = ([Fhelp], Nothing)
+  | "-v" `elem` xs || "--version" `elem` xs = ([Fversion], Nothing)
   | otherwise = (mapMaybe parseFlag $ init xs, Just $ last xs)
 
 getFiles :: FilePath -> IO [FilePath]
@@ -121,6 +128,7 @@ main =
   where handleArgs :: ([CliFlag], Maybe String) -> IO ()
         handleArgs (options, userPath)
           | Fhelp `elem` options = printHelp
+          | Fversion `elem` options = putStrLn ("Klassco version: 7.1.0")
           | userPath == Nothing = printMessage forgotPathMsg
           | otherwise = do
               let defaultExt = ["html"]
